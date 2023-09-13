d8015856eb0667a4ccda52583b8cfcd52b27934e
undertap <raf@undertap.com>
2023-09-13 20:22:02 +0000

    Fixed the SUPABASE_SERVICE_ROLE_KEY issue on 2 files

--- app/leap/image-webhook/route.ts
+++ app/leap/image-webhook/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from 'next/server';
 export const dynamic = 'force-dynamic'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseServiceRoleKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 const leapApiKey = process.env.LEAP_API_KEY;
 const leapWebhookSecret = process.env.LEAP_WEBHOOK_SECRET;
 
